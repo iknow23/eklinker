c42e93243ea2b3522a4c4c0b216bd2cab36ae6b3
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-02-08 18:56:47 +0300

    Перписал js код для регионов в шапке

--- source/js/main.js
+++ source/js/main.js
@@ -8,7 +8,7 @@ var contactNumbers = ['8(495)324-00-04', '8(863)310-01-02', '8(495)324-00-04', '
 area1.addEventListener('click', function(evt) {
   text.innerText = contactNumbers[0];
   area1.style.opacity = '1';
-  if (area2.style.opacity = '1' || area3.style.opacity = '1' || area4.style.opacity = '1') {
+  if ( (area2.style.opacity = '1') || (area3.style.opacity = '1') || (area4.style.opacity = '1') ) {
     area2.style.opacity = '0.5';
     area3.style.opacity = '0.5';
     area4.style.opacity = '0.5';
@@ -18,7 +18,7 @@ area1.addEventListener('click', function(evt) {
 area2.addEventListener('click', function(evt) {
   text.innerText = contactNumbers[1];
   area2.style.opacity = '1';
-  if (area1.style.opacity = '1' || area3.style.opacity = '1' || area4.style.opacity = '1') {
+  if ( (area1.style.opacity = '1') || (area3.style.opacity = '1') || (area4.style.opacity = '1') ) {
     area1.style.opacity = '0.5';
     area3.style.opacity = '0.5';
     area4.style.opacity = '0.5';
@@ -28,7 +28,7 @@ area2.addEventListener('click', function(evt) {
 area3.addEventListener('click', function(evt) {
   text.innerText = contactNumbers[2];
   area3.style.opacity = '1';
-  if (area1.style.opacity = '1' || area2.style.opacity = '1' || area4.style.opacity = '1') {
+  if ( (area1.style.opacity = '1') || (area2.style.opacity = '1') || (area4.style.opacity = '1') ) {
     area1.style.opacity = '0.5';
     area2.style.opacity = '0.5';
     area4.style.opacity = '0.5';
@@ -38,7 +38,7 @@ area3.addEventListener('click', function(evt) {
 area4.addEventListener('click', function(evt) {
   text.innerText = contactNumbers[3];
   area4.style.opacity = '1';
-  if (area1.style.opacity = '1' || area2.style.opacity = '1' || area3.style.opacity = '1') {
+  if ( (area1.style.opacity = '1') || (area2.style.opacity = '1') || (area3.style.opacity = '1') ) {
     area1.style.opacity = '0.5';
     area2.style.opacity = '0.5';
     area3.style.opacity = '0.5';
